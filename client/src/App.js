ef04ed5e3318542f837fbca43541000b24c46661
Justin Ryan <=>
2025-05-10 20:35:56 -0400

    update auth url

--- client/src/App.js
+++ client/src/App.js
@@ -65,7 +65,7 @@ function App() {
 
       {!auth && (
         <div className="flex justify-center">
-          <a href="http://localhost:3000/auth/strava">
+          <a href="/auth/strava">
             <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-4 rounded shadow">
               Connect with Strava
             </button>
